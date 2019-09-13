1727dd9b516ff7db092bf77108f76bac46b4b31f
cedric e goddard <8ceg6@comcast.net>
2019-09-13 19:49:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ end
 introduction("Josh")
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learniing to program in #{language}."
+  puts "Hi my name is #{name} and I am learniing to program in #{language}.js."
 end  
 introduction_with_language("Dan", "Ember")
